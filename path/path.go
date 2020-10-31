b9651f16b0b8bd3a099b839507c10a401da64422
Alan Cesar Elias <alancesarelias@gmail.com>
2020-10-31 12:26:39 -0300

    Allowing multiple mime types

--- path/path.go
+++ path/path.go
@@ -24,11 +24,17 @@ func NewBuilder() *Builder {
 }
 
 // LookFor deep walks in a path and get all files that match with a provided mime.Type.
-func LookFor(rootPath string, t mime.Type) []string {
+func LookFor(rootPath string, types ...mime.Type) []string {
 	paths := make([]string, 0)
 	_ = filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if info != nil && !info.IsDir() && info.Mode().IsRegular() && mime.Is(path, t) {
-			paths = append(paths, path)
+		if info == nil || info.IsDir() || !info.Mode().IsRegular() {
+			return nil
+		}
+
+		for index := range types {
+			if mime.Is(path, types[index]) {
+				paths = append(paths, path)
+			}
 		}
 
 		return nil
