34ce05adac290ef9f1ebb386c180567955b91f1e
Alan Cesar Elias <alancesarelias@gmail.com>
2020-10-31 13:38:22 -0300

    Renaming constants

--- mime/mime.go
+++ mime/mime.go
@@ -8,10 +8,10 @@ import (
 type Type string
 
 const (
-	ApplicationOctetStream Type = "application/octet-stream"
-	Application            Type = "application/"
-	AudioType              Type = "audio/"
-	ImageType              Type = "image/"
+	ApplicationOctetStreamType Type = "application/octet-stream"
+	ApplicationType            Type = "application/"
+	AudioType                  Type = "audio/"
+	ImageType                  Type = "image/"
 )
 
 // Is checks if a file is a provide Type.
