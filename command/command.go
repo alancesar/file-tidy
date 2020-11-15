735a90923a15e9ad2f2046448a0d7568da6c68a3
Alan Cesar Elias <alancesarelias@gmail.com>
2020-11-15 01:15:59 -0300

    Renaming method

--- command/command.go
+++ command/command.go
@@ -8,8 +8,8 @@ import (
 
 type Command func(source, destination string) error
 
-// MkDirCommand create a new directory if it does not exist.
-func MkDirCommand(_, destination string) error {
+// MkDir create a new directory if it does not exist.
+func MkDir(_, destination string) error {
 	dir, _ := filepath.Split(destination)
 	return os.MkdirAll(dir, os.ModePerm)
 }
