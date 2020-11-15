0676f30ded88bd6323d3e04b35caf849fd67910a
Alan Cesar Elias <alancesarelias@gmail.com>
2020-11-15 01:08:15 -0300

    Adding CopyFile method

--- command/command.go
+++ command/command.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -11,4 +12,14 @@ type Command func(source, destination string) error
 func MkDirCommand(_, destination string) error {
 	dir, _ := filepath.Split(destination)
 	return os.MkdirAll(dir, os.ModePerm)
+}
+
+// CopyFile copies the file.
+func CopyFile(source, destination string) error {
+	input, err := ioutil.ReadFile(source)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(destination, input, 0644)
 }
